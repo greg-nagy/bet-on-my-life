2782f452e1a9bc71602bcc77eb62b166110a3d7d
Nagy Gergo <nagy.gergo@3pixel.hu>
2020-08-26 20:02:03 +0200

    remove auth routing module for storybook to work

--- libs/feature-auth/src/lib/feature-auth.module.ts
+++ libs/feature-auth/src/lib/feature-auth.module.ts
@@ -4,20 +4,26 @@ import { LoginComponent } from './containers/login/login.component';
 import { RegistrationComponent } from './containers/registration/registration.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
-import { FeatureAuthRoutingModule } from './feature-auth-routing.module';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    FeatureAuthRoutingModule,
     CardModule,
     InputTextModule,
     ButtonModule,
     ReactiveFormsModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [
     LoginComponent,
